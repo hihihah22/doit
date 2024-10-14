933f768cb0ef24f298cac1be201aeaf45e5d032d
hihihah22 <88105325+hihihah22@users.noreply.github.com>
2024-10-14 21:13:34 +0700

    Add files via upload

---
+++ doit.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+
+
+void hihihah(){
+    
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    #define orz "mo"
+    if(fopen(orz".inp", "r")){
+        freopen(orz".inp", "r", stdin);
+        freopen(orz".out", "w", stdout);
+    }
+
+    int t = 1;
+    // cin >> t;
+    while(t --){
+        hihihah();
+    }
+    cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s";
+    return 0;
+}
